c4088374c1fbcedac070e97e77659f2d23f91a93
yuyuko0316 <yuyuko0316@gmail.com>
2017-07-18 23:46:29 +0800

    添加项目文件。

---
+++ Project2/test.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+int main()
+{
+	cout << "hello world" << endl;
+	system("pause");
+	return 0;
+}
